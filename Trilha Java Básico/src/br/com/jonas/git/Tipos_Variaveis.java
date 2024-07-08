5451298fe0e64b10a3c082918383961bea454d54
Jonas Pereira Alves <jonaspereira4321@gmail.com>
2024-07-08 11:45:20 -0300

    Adicionei arquivos via upload

---
+++ Trilha Java Básico/src/br/com/jonas/git/Tipos_Variaveis.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.jonas.git;
+
+/**
+ * 
+ */
+public class Tipos_Variaveis {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Olá tudo bem?");
+
+	}
+
+}

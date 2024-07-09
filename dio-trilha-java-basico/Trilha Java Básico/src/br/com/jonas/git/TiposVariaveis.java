4c67527b5d3cee3ea8e6a3465c1a1364c3fa2cc4
Jonas Pereira Alves <jonaspereira4321@gmail.com>
2024-07-08 21:36:52 -0300

    Add files via upload
    upload

---
+++ dio-trilha-java-basico/Trilha Java Básico/src/br/com/jonas/git/TiposVariaveis.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package br.com.jonas.git;
+
+/**
+ * 
+ */
+public class TiposVariaveis {
+
+	
+	public static void main(String[] args) {
+		
+		int idade = 29;
+		double altura = 1.56;
+		boolean branco = false;
+		
+		
+		System.out.println("Olá tudo bem?");
+		System.out.println("Jonas");
+		System.out.println(idade);
+		System.out.println(altura);
+		System.out.println(branco);
+
+	}
+
+}

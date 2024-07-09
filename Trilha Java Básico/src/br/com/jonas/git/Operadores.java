8d35cde622693a277cccefd0ba602b925e208a93
JonasPAlves <jonaspereira4321@gmail.com>
2024-07-08 22:00:30 -0300

    Adicionando a classe de Operadores

---
+++ Trilha Java Básico/src/br/com/jonas/git/Operadores.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package br.com.jonas.git;
+
+/**
+ * 
+ */
+public class Operadores {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String nome = "Jonas"; // oerador de atribuição
+		int idade = 28;
+		
+		int somaIdade = 28 + 1;
+		int subtracaoIdade = 30 - 1;
+		int multiplicacaoIdade = 29 * 1;
+		int divisaoIdade = 29 / 1;
+		double soma = 15.6 + 12.4;
+		int modulo = 18 % 3; // resto da divisão
+		double resultado = (12*8) + (10/7)-(8+5);
+		String nomeCompleto = "Jonas " + "Pereira " + "Alves";
+		
+		System.out.println(nomeCompleto);
+		
+	}
+
+}
